5111b317a67bd9ded01b7cfa3dad01915b379000
Jon Cotton <jc@finaltransfer.net>
2014-01-21 19:19:21 -0800

    Django 1.4+ URL imports

--- armstrong/core/arm_content/tests/arm_content_support/urls.py
+++ armstrong/core/arm_content/tests/arm_content_support/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+try:
+    from django.conf.urls import patterns, include, url
+except ImportError:  # Django 1.3
+    from django.conf.urls.defaults import patterns, include, url
 
 
 admin.autodiscover()
